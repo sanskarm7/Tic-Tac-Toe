46c15b7eff62c110cc07b6a0a42db18543b5856a
Sanskar Mishra <sanskar.mishra@gmail.com>
2023-05-13 14:16:37 -0500

    finished base game of tic tac toe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pprint
+import random
 
 
 def main():
@@ -7,7 +7,7 @@ def main():
         [None, None, None],
         [None, None, None]
     ]
-    pprint.pprint(board)
+    display_board(board)
 
     turn = True
 
@@ -15,28 +15,58 @@ def main():
         if turn:
             player_turn(board)
 
+
         else:
             comp_turn(board)
-        print_board(board)
+        display_board(board)
+
+        turn = not turn
+
+    if winner(board) == 'x':
+        print("Player won")
+    elif winner(board) == 'o':
+        print("Computer Won")
+    else:
+        print("stalemate")
 
 
 def winner(board):
-    pass
+    for row in board:
+        if row[0] == row[1] and row[1] == row[2] and row[1] is not None:
+            return row[1]
+    for j in range(len(board)):
+        if board[0][j] == board[1][j] and board[1][j] == board[2][j] and board[1][j] is not None:
+            return board[1][j]
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[1][1] is not None:
+        return board[1][1]
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[1][1] is not None:
+        return board[1][1]
+    for row in board:
+        for val in row:
+            if val is None:
+                return None
+    return "s"
 
 
 def player_turn(board):
-    choice = input("Choose your move: ")
+    choice = input("Choose your move: ").upper()
     row = ord(choice[0]) - 65
     col = ord(choice[1]) - ord("1")
     board[row][col] = 'x'
 
 
 def comp_turn(board):
-    pass
+    while True:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+        if board[row][col] is None:
+            board[row][col] = 'o'
+            return
 
 
-def print_board(board):
-    pprint.pprint(board)
+def display_board(board):
+    for row in board:
+        print(row)
 
 
 if __name__ == '__main__':
